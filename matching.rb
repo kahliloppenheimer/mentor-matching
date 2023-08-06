b2e8bba2107d814925899c4320d38a3fc0d73cc1
Kahlil Oppenheimer <kahlil@stripe.com>
2023-08-06 12:48:02 -0400

    Add basic people struct parsing from CSV

--- matching.rb
+++ matching.rb
@@ -6,10 +6,29 @@ require 'csv'
 class Matching
   extend T::Sig
 
+  NameCol = 'name'
+  CityCol = 'city'
+  StateCol = 'state'
+  RegionCol = 'region'
+  SeniorityCol = 'seniority'
+
+  class Person < T::Struct
+    const :name, String
+    const :city, String
+    const :state, String
+    const :region, String
+    const :seniority, String
+  end
+
   sig {params(csv_path: String).void}
   def match(csv_path)
     rows = parse_csv(csv_path)
-    puts(rows.to_s)
+    headers = rows[0]
+    csv_schema = parse_csv_schema(T.must(headers))
+    people = T.must(rows[1..]).map { |row| parse_person(csv_schema, row) }
+
+    puts("Headers:\n#{headers.to_s}")
+    puts("\nPeople:\n#{people.map(&:serialize).join("\n")}")
   end
 
   sig do
@@ -26,7 +45,40 @@ class Matching
       end
     end
   end
+
+  sig {params(headers: T::Array[String]).returns(T::Hash[String, Integer])}
+  private def parse_csv_schema(headers)
+    schema = T.let({}, T::Hash[String, Integer])
+    headers.each_with_index do |header, i|
+      schema_val = case header
+      when NameCol,
+        CityCol,
+        StateCol,
+        RegionCol,
+        SeniorityCol
+        i
+      else
+        raise 'Found unexpected CSV header: #{header}'
+      end
+
+      schema[header] = i
+    end
+    schema
+  end
+
+  sig do
+    params(schema: T::Hash[String, Integer], row: T::Array[String]).returns(Person)
+  end
+  private def parse_person(schema, row)
+    Person.new(
+      name: row.fetch(schema.fetch(NameCol)),
+      city: row.fetch(schema.fetch(CityCol)),
+      state: row.fetch(schema.fetch(StateCol)),
+      region: row.fetch(schema.fetch(RegionCol)),
+      seniority: row.fetch(schema.fetch(SeniorityCol)),
+    )
+  end
 end
 
 matching = Matching.new
-matching.match('test.csv')
+matching.match('test-inputs.csv')
