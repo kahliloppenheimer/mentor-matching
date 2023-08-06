2bee1fcea901c1fdf614c1e99c7d4528511cbdb4
Kahlil Oppenheimer <kahlil@stripe.com>
2023-08-06 12:01:20 -0400

    Add example matching class that just parses and prints CSV

--- matching.rb
+++ matching.rb
@@ -1,16 +1,32 @@
 # typed: strict
 require 'sorbet-runtime'
+require 'csv'
 
 # Example matching class!
 class Matching
   extend T::Sig
 
-  sig do
-    params(a: Integer, b: Integer).returns(Integer)
+  sig {params(csv_path: String).void}
+  def match(csv_path)
+    rows = parse_csv(csv_path)
+    puts(rows.to_s)
   end
-  def self.plus(a, b)
-    a + b
+
+  sig do
+    params(csv_path: String).returns(T::Array[T::Array[String]])
+  end
+  private def parse_csv(csv_path)
+    CSV.read(csv_path).map do |row|
+      row.map do |value|
+        new_value = value&.downcase&.strip
+        if new_value.nil? || new_value.empty?
+          raise "Found empty value while parsing CSV. row=#{row}"
+        end
+        new_value
+      end
+    end
   end
 end
 
-puts(Matching.plus(1, 2))
+matching = Matching.new
+matching.match('test.csv')
